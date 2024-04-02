d7a5a79b3bef73ca312f050600c70a6c3dea8aec
Wei Zhou <wei.zhou@partner.bmw.com>
2024-04-03 00:25:09 +0800

    fix

--- scripts/start_WoL.sh
+++ scripts/start_WoL.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 source "$HOME/little_dove_AI_bot/scripts/send_dingding_message.sh"
-for i in {1..10}
-do
-    "$HOME/little_dove_AI_bot/scripts/WoL.sh" 04:7C:16:7B:0E:0D 192.168.28.205
-done
+wol 04:7C:16:7B:0E:0D
 send_dingding_message "WOL message sent!"
