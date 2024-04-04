ff6393552ed03941a13124b1dd917ccc2399dbc8
Wei Zhou <wei.zhou@partner.bmw.com>
2024-04-04 22:55:59 +0800

    1

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source $HOME/little_dove_AI_bot/scripts/send_dingding_message.sh
-COMMAND="shutdown -s -t 60"
+COMMAND="shutdown -s -t 10"
 
 ssh "administrator@192.168.28.205" "$COMMAND"
 
