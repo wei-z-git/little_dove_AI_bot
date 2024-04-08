f3d5ea316cf569cae171fd1e69d638531f038906
wei-z <32572815+wei-z-git@users.noreply.github.com>
2024-04-08 23:31:06 +0800

    Update config.py

--- qq-group-admin-plugin/config.py
+++ qq-group-admin-plugin/config.py
@@ -13,7 +13,7 @@ class Config(BaseModel, extra=Extra.ignore):
     sd_host: str = 'frp.runtime20.space'  # sd的host地址
     sd_port: int = 52097  # sd server的端口
     sd_groups: list = ["1170115778", "617875321",
-                       "190842825", "689583064","715025692","1141560393","810575702"]  # 每天需要发送sd ai图片的groups
+                       "190842825", "689583064","715025692"]  # 每天需要发送sd ai图片的groups
     sd_hour: int = 2  # 每天需要发送sd ai图片时间，小时
     sd_minute: int = 59  # 每天需要发送sd ai图片时间，分钟
 
