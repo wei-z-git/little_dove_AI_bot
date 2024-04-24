7ea79c69aa79d7a95c225dcdf797f79b715bf076
wei-z-git <Amadeus.LU.Rui@outlook.com>
2024-04-24 20:30:36 +0800

    fix

--- qq-group-message-summary/config.py
+++ qq-group-message-summary/config.py
@@ -7,7 +7,7 @@ class Config(BaseModel, extra=Extra.ignore):
     ai_api_key: str  # llama中文社区 api key
     ai_secret_key: str  # llama中文社区 secret key
     exclude_user_list: list = [3320741388,2024085613] # 排除用户的id
-    keywords_file_path: str = "./config/filter_keywords.txt" # 过滤关键词的文件路径
+    keywords_file_path: str = "config/filter_keywords.txt" # 过滤关键词的文件路径
 
 
 driver = get_driver()
