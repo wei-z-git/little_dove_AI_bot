1102b0b239cd2b5813e6381bb1532cc5f672aad6
wei-z-git <Amadeus.LU.Rui@outlook.com>
2024-04-25 23:02:18 +0800

    add half day chat

--- qq-group-message-summary/summary.py
+++ qq-group-message-summary/summary.py
@@ -18,6 +18,9 @@ from nonebot_plugin_session import extract_session, SessionIdType
 matcher_summary = on_command(
     '今日群聊', priority=3, permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER)
 
+matcher_summary_half_day = on_command(
+    '半天群聊', priority=2, permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER)
+
 matcher_product_test = on_command(
     'test', priority=3, permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER)
 
@@ -43,3 +46,23 @@ async def _(matcher: Matcher, bot: Bot, event: GroupMessageEvent,):
         await matcher.send(str(ai_summary))
         await matcher.send(f"used token:{used_token}")
 
+@matcher_summary_half_day.handle()
+async def _(matcher: Matcher, bot: Bot, event: GroupMessageEvent,):
+    session = extract_session(bot, event)
+    records = await get_message_records(
+        session=session,
+        id_type=SessionIdType.GROUP,
+        time_start=datetime.now() - timedelta(days=0.5),
+    )
+    summary = Summary(plugin_config)
+    records_merged = await summary.filter(records)
+    if records_merged == "":
+        await matcher.send("没有足够的数据")
+    else:
+        total_length = sum(len(word) for word in records_merged)
+        await matcher.send(f"message length : {total_length}")
+        response = await summary.get_ai_message_res(records_merged)
+        used_token=response.usage
+        ai_summary=response.choices[0].message.content
+        await matcher.send(str(ai_summary))
+        await matcher.send(f"used token:{used_token}")
