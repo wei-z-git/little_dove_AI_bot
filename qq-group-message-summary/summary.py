26149823d638bd44e4dd685c2aaccc0369ef0970
wei-z-git <Amadeus.LU.Rui@outlook.com>
2024-04-25 21:11:25 +0800

    fix order

--- qq-group-message-summary/summary.py
+++ qq-group-message-summary/summary.py
@@ -35,6 +35,6 @@ async def _(matcher: Matcher, bot: Bot, event: GroupMessageEvent,):
     if records_merged == "":
         await matcher.send("没有足够的数据")
     else:
-        ai_summary = await summary.get_ai_message_res(records_merged)
         await matcher.send(len(records_merged))
+        ai_summary = await summary.get_ai_message_res(records_merged)
         await matcher.send(str(ai_summary))
