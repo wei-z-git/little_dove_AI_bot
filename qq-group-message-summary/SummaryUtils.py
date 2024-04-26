ed68cce3b1855f6b85531a71a05b534c80a042f7
wei-z <32572815+wei-z-git@users.noreply.github.com>
2024-04-26 21:56:38 +0800

    Update SummaryUtils.py

--- qq-group-message-summary/SummaryUtils.py
+++ qq-group-message-summary/SummaryUtils.py
@@ -21,7 +21,7 @@ class Summary:
         content = "如下是一段多个用户参与的聊天记录，换行符'\n'代表一条消息的终结，请提取有意义的词句，总结这段聊天记录,字数在300字以内:" + message
         # content_list = await self.content_cutting(content)
         response = self.client.chat.completions.create(
-            model="Atom-13B-Chat",
+            model="Llama3-Chinese-8B-Instruct",
             messages=[
                 {"role": "user", "content": content}
             ],
