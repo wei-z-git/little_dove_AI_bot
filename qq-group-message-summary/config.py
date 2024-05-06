f43c0a06828d0574283ad76c29d03ee78ab72050
wei-z <32572815+wei-z-git@users.noreply.github.com>
2024-05-06 21:44:52 +0800

    add schedule job for daily chat

--- qq-group-message-summary/config.py
+++ qq-group-message-summary/config.py
@@ -8,9 +8,10 @@ class Config(BaseModel, extra=Extra.ignore):
     ai_secret_key: str  # llama中文社区 secret key
     exclude_user_list: list = [3320741388,2024085613] # 排除用户的id
     keywords_file_path: str = "config/filter_keywords.txt" # 过滤关键词的文件路径
-    ai_summary_hour: int = 2  # 每天需要发送ai总结时间，小时
-    ai_summary_minite: int = 59  # 每天需要发送ai总结时间，分钟
-
+    ai_summary_hour: int = 22  # 每天需要发送ai总结时间，小时
+    ai_summary_minite: int = 00  # 每天需要发送ai总结时间，分钟
+    ai_summary_groups: list = ["1170115778", "617875321",
+                       "190842825", "689583064","715025692"]  # 每天需要发送ai总结的groups
 driver = get_driver()
 global_config = driver.config
 plugin_config = Config.parse_obj(global_config)
